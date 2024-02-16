9b5bbaf844dc0441f79fa6114ef73e96555d3875
Salman <sallu123xx@gmail.com>
2024-02-16 14:56:17 +0530

    Modified wi
    Added WhatsappMessage class

--- controllers/ProductController.mjs
+++ controllers/ProductController.mjs
@@ -1,5 +1,9 @@
+import dotenv from "dotenv";
+dotenv.config();
+
 import ProductModel from "../models/ProductModel.mjs";
-import sendMessage from "../wi/sendMessage.mjs";
+import WhatsappMessage from "../wi/WhatsappMessage.mjs";
+// import sendMessage from "../wi/sendMessage.mjs";
 
 export async function fetchFeaturedProducts() {
     try {
@@ -30,14 +34,57 @@ export async function sendWhatsapp(req, res) {
     });
     if(!productFound) res.sendStatus(404);
     else {
+        const username = "Salman";
         const product = productFound[0];
+        const to = process.env.WHATSAPP_TO;
+        
         // Send whatsapp message
-        sendMessage(product)
-            .then(response => {
-                res.status(200).json({msg: "Whatsapp message send", response});
-            })
-            .catch(err => {
-                res.status(500).json({msg: "Cannot send message", err: err});
-            });
+        const whatsappMessage = new WhatsappMessage();
+        const [ response, err ] = await whatsappMessage.sendTemplateMessage({
+            to,
+            messageHeader: {
+                type: "header",
+                parameters: [
+                    {
+                        type: "text",
+                        text: product.name // Product name
+                    }
+                ]
+            },
+            messageBody: {
+                type: "body",
+                parameters: [
+                    {
+                        type: "text",
+                        text: username // User name
+                    },
+                    {
+                        type: "text",
+                        text: product.description.join("\\n") // Product Description
+                    }
+                ]
+            },
+            messageButton: {
+                type: "button",
+                sub_type: "url", // Button Type
+                index: 0, // Button Index
+                parameters: [
+                    {
+                        type: "text",
+                        text: product.slug 
+                    }
+                ]
+            }
+        });
+        if(response) res.status(200).json({msg: "Whatsapp message send", response});
+        else res.status(500).json({msg: "Cannot send message", err: err});
+        // sendMessage(product)
+        //     .then(response => {
+        //         res.status(200).json({msg: "Whatsapp message send", response});
+        //     })
+        //     .catch(err => {
+        //         res.status(500).json({msg: "Cannot send message", err: err});
+        //     });
+
     }
 }
--- wi/WhatsappMessage.mjs
+++ wi/WhatsappMessage.mjs
@@ -1,9 +1,7 @@
 import { dirname } from "node:path"; // buildin module from `node:` schema
 import { fileURLToPath } from 'node:url';
-import path from "path";
-import fs from "fs";
 
-import got from "got";
+// import got from "got";
 import axios from "axios";
 import dotenv from "dotenv";
 dotenv.config();
@@ -13,12 +11,11 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 
-
 // HTTP HEADER
-const httpHeaders = {
-  Authorization: `Bearer ${process.env.WHATSAPP_AUTH_TOKEN}`,
-  "Content-Type": "application/json"
-}
+// const httpHeaders = {
+//     Authorization: `Bearer ${process.env.WHATSAPP_AUTH_TOKEN}`,
+//     "Content-Type": "application/json"
+// }
 
 // HTTP BODY
 const httpBody = {
@@ -69,8 +66,75 @@ const httpBody = {
     }
 }
 
+export default class WhatsappMessage {
+    static httpHeaders = {
+        Authorization: `Bearer ${process.env.WHATSAPP_AUTH_TOKEN}`,
+        "Content-Type": "application/json"
+    };
+    
+    constructor() {
+        this.httpBody = {
+            messaging_product: "whatsapp",
+            recipient_type: "individual",
+            to: process.env.WHATSAPP_TO,
+            type: "template",
+            template: {
+                name: "test_template",
+                language: {
+                    code: "en_US"
+                },
+                components: []
+            }
+        };
+    }
 
-export default async (product) => {
+    // static messageHeader = {
+    //     type: "header",
+    //     parameters: messageHeaderParams
+    // };
+    // static messageBody = {
+    //     type: "body",
+    //     parameters: messageBodyParams
+    // };
+    // static messageButton = {
+    //     type: "button",
+    //     sub_type: "url", // Button Type
+    //     index: 0, // Button Index
+    //     parameters: messageButtonParams
+    // };
+
+    async sendTemplateMessage({ template, to, messageHeader, messageBody, messageButton}) {
+        if(template) this.httpBody.template.name = template;
+        if(to) this.httpBody.to = to;
+        
+        if(messageHeader) this.httpBody.template.components.push(messageHeader);
+        if(messageBody) this.httpBody.template.components.push(messageBody);
+        if(messageButton) this.httpBody.template.components.push(messageButton);
+        
+
+        try {
+            const result = await axios.post(process.env.WHATSAPP_URL, JSON.stringify(this.httpBody), { headers: WhatsappMessage.httpHeaders });
+            return [result.data, null];
+        }catch(err) {
+            return [null, err.response.data];
+        }
+
+
+        // return new Promise(async (resolve, reject) => {
+        //     try{
+        //         const result = await axios.post(process.env.WHATSAPP_URL, JSON.stringify(this.httpBody), { headers: WhatsappMessage.httpHeaders })
+        //         resolve(result.data);
+        //     }catch(err) {
+        //         reject(err.response.data);
+        //     }
+        // });
+        
+    }
+}
+
+
+
+export async function sendMessage (product) {
     const username = "Salman";
     const { slug, name, description } = product;
 
