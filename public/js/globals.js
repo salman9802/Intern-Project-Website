62590f309bbf5b950ca6014ae23a8393b5170ff8
Salman <sallu123xx@gmail.com>
2024-03-15 16:11:40 +0530

    Added Cart Menu UI
    - Fixed overlay
    - Moved hamburger.js logic into globals.js

--- public/js/globals.js
+++ public/js/globals.js
@@ -1,10 +1,4 @@
-const notificationAlerts = document.querySelectorAll(".notification-alert");
-notificationAlerts?.forEach(notificationAlert => {
-    setTimeout(() => {
-        notificationAlert.remove();
-    }, 2500);
-});
-
+// HEADER SEARCH INPUT
 // https://stackoverflow.com/questions/6234773/can-i-escape-html-special-chars-in-javascript
 const escapeHtml = (unsafe) => {
     return unsafe.replaceAll('&', '&amp;').replaceAll('<', '&lt;').replaceAll('>', '&gt;').replaceAll('"', '&quot;').replaceAll("'", '&#039;');
@@ -40,4 +34,39 @@ document.getElementById("header-search-input").addEventListener("keyup", async e
 document.getElementById("header-search-cancel").addEventListener("click", e => {
     headerSearch.classList.remove("flex");
     headerSearch.classList.add("hidden");
+});
+
+
+// HAMBURGER MENU
+const openHamburgerBtn = document.getElementById("open-hamburger-btn");
+const closeHamburgerBtn = document.getElementById("close-hamburger-btn");
+const hamburgerMenu = document.getElementById("hamburger-menu");
+
+openHamburgerBtn.addEventListener("click", e => {
+    document.body.style.overflowY = "hidden";
+    hamburgerMenu.classList.add("hamburger--open");
+    document.getElementById("hamburger-overlay").classList.remove("hidden");
+});
+
+closeHamburgerBtn.addEventListener("click", e => {
+    hamburgerMenu.classList.remove("hamburger--open");
+    document.getElementById("hamburger-overlay").classList.add("hidden");
+    document.body.style.overflowY = "auto";
+});
+
+// CART
+const openCartBtn = document.getElementById("open-cart-btn");
+const closeCartBtn = document.getElementById("close-cart-btn");
+const cartMenu = document.getElementById("cart-menu");
+
+openCartBtn.addEventListener("click", e => {
+    document.body.style.overflowY = "hidden"; // disable scroll
+    cartMenu.classList.add("cart--open"); // open cart
+    document.getElementById("cart-overlay").classList.remove("hidden"); // show overlay
+});
+
+closeCartBtn.addEventListener("click", e => {
+    cartMenu.classList.remove("cart--open"); // close cart
+    document.getElementById("cart-overlay").classList.add("hidden"); // hide overlay
+    document.body.style.overflowY = "auto"; // enable scroll
 });
